a4f46541ef26bf4a23bae42d5087d6f3711e7794
tommaso auerbach <tsa302@nyu.edu>
2019-06-14 20:32:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,14 @@
 def countdown_to_midnight
+
   counter = 0
+
   while counter < 10
     puts "#{counter} SECOND(S)!"
     counter += 1
   end
+
   if counter == 12 do
   puts "HAPPY NEW YEAR!"
 end
+
 end
