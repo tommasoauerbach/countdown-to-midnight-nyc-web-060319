e4ca447fa1c665849e8c14292e5b151aaff7baaf
tommaso auerbach <tsa302@nyu.edu>
2019-06-14 20:43:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown (seconds_to_midnight)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
