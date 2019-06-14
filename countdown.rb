4ac2475b1419cb7932063d1956addc9dab5003b1
tommaso auerbach <tsa302@nyu.edu>
2019-06-14 20:43:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,17 @@
+def countdown (seconds_to_midnight)
 
-  x = 10
-  while x > 1
-    puts "#{x} SECOND(S)!"
-    x -= 1
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
